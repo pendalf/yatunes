c306edf12b243dc441825d63159fb7af883770a9
Zander <akma_2003@mail.ru>
2020-12-23 18:46:25 +0300

    Третий день интенсива, домашнее задание: Добавлено управление громкостью радиоплеера

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -7,6 +7,10 @@ export const radioPlayerInit = () => {
     const raradioHeaderBig = document.querySelector('.radio-header__big');
     const radioStop = document.querySelector('.radio-stop');
     const radioNavigation = document.querySelector('.radio-navigation');
+    const radioVolume = document.querySelector('.radio-volume');
+    const radioVolumeMute = document.querySelector('.radio-volume__mute');
+    const radioVolumeDown = document.querySelector('.radio-volume__down');
+    const radioVolumeUp = document.querySelector('.radio-volume__up');
 
     // Конструктор аудио
     const audio = new Audio();
@@ -33,6 +37,51 @@ export const radioPlayerInit = () => {
         radioItem.forEach(el => el.classList.remove('select'));
         elem.classList.add('select');
     };
+
+
+    // Изменение громкости
+    const changeValume = (direction = 0) => {
+        if (direction !== 0) {
+            radioVolume.value = +radioVolume.value + (direction * 10);
+            if (radioVolume.value > 100) {
+                radioVolume.value = 100;
+            } else if (radioVolume.value < 0) {
+                radioVolume.value = 0;
+            }
+        }
+        const valueVolume = radioVolume.value;
+        audio.volume = valueVolume / 100;
+        toggleMuteIcon();
+    };
+
+    // Реакция иконки включения/выключения звука
+    const toggleMuteIcon = () => {
+        if (radioVolume.value > 0) {
+            radioVolumeMute.classList.remove('disabled');
+        } else {
+            radioVolumeMute.classList.add('disabled');
+        }
+    };
+
+    // Включение/выключение звука
+    const toggleMute = () => {
+        if (radioVolume.value > 0) {
+            radioVolume.setAttribute('data-old-volume', radioVolume.value);
+            radioVolume.value = 0;
+            audio.volume = 0;
+        } else {
+            let volumeOld = radioVolume.getAttribute('data-old-volume');
+            if (volumeOld === null) {
+                radioVolume.setAttribute('data-old-volume', 0);
+                volumeOld = 0;
+            }
+            radioVolume.value = volumeOld;
+            audio.volume = volumeOld / 100;
+        }
+        toggleMuteIcon();
+    };
+
+
 
     //  Переключение радиостанций
     radioNavigation.addEventListener('change', event => {
@@ -62,5 +111,25 @@ export const radioPlayerInit = () => {
             audio.pause();
         }
         toggleIcon();
+    });
+
+    // Остановка проигрвывния радио, если таба с проигрывателем закрылась
+    document.body.addEventListener('click', () => {
+        if (!radio.closest('.player-block').classList.contains('active')) {
+            audio.pause();
+        }
+    });
+
+    // Управление громкостью
+    changeValume();
+    radioVolume.addEventListener('input', () => {
+        changeValume();
+    });
+    radioVolumeMute.addEventListener('click', toggleMute);
+    radioVolumeDown.addEventListener('click', () => {
+        changeValume(-1);
+    });
+    radioVolumeUp.addEventListener('click', () => {
+        changeValume(1);
     });
 };
